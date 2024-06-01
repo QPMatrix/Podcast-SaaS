d307b037a4e0fd71fcc737027bed5bf5cf4bfcdf
QPM <admin@QuantumPinnacleMatrix.tech>
2024-06-01 19:59:57 +0300

    adding demo data and ui of home page at this moment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import PodcastCard from "@/components/podcast-card";
+import { podcastData } from "@/constants";
 import React from "react";
 
 const Home = () => {
@@ -5,6 +7,17 @@ const Home = () => {
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcast</h1>
+        <div className="podcast_grid">
+          {podcastData.map(({ id, imgURL, title, description }) => (
+            <PodcastCard
+              key={id}
+              imgURL={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
