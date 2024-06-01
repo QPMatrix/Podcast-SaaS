c3d4d999429ae5c0d7ce086c54c2c3807c16361f
QPM <admin@QuantumPinnacleMatrix.tech>
2024-06-01 19:15:25 +0300

    adding left and right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,34 @@
+import LeftSidebar from "@/components/left-sidebar";
+import MobileNav from "@/components/mobile-nav";
+import RightSidebar from "@/components/right-sidebar";
+import Image from "next/image";
 import React from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div>
-      <main>
-        <p className="text-white-1">Left</p>
-        {children}
-        <p className="text-white-1">Right</p>
+    <div className=" relative flex flex-col">
+      <main className=" relative flex bg-black-3">
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src={"/icons/logo.svg"}
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
+              <MobileNav />
+            </div>
+            <div className="flex flex-col md:pb-14">
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSidebar />
       </main>
     </div>
   );
