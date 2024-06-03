7e58c29f947ed0d05746b8894c14700b8abe85b0
QPM <admin@QuantumPinnacleMatrix.tech>
2024-06-03 09:42:27 +0300

    done withe logic and the ui at this moment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ import React from "react";
 const Home = () => {
   const trendingPodcast = useQuery(api.podcast.getTrendingPodcasts);
   return (
-    <div className="mt-9 flex flex-col gap-9">
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcast</h1>
         <div className="podcast_grid">
